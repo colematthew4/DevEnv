40f773fc665138101fe5f9ea60c9dccfe697f838
Matthew Cole <colematthew4@gmail.com>
2021-08-22 19:27:11 -0700

    chore: Cleans up install script

--- install.zsh
+++ install.zsh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+git remote add https://github.com/colematthew4/dotfiles.git
+git checkout origin/main
+
 # First order of business: configure repo directory if not already set
 if [ -z "$REPOS_DIR" ]; then
   export REPOS_DIR=$HOME/Repos/DevEnv
@@ -14,5 +17,11 @@ fi
 # Use version managers from brew to install actual platform binaries
 . $REPOS_DIR/apps/node
 
+# Configure vscode and xcode settings
+. $REPOS_DIR/apps/vscode
+. $REPOS_DIR/apps/xcode
+
 # Invoke scripts to set system preferences and other actions that can't be done by zsh
-# osascript $REPOS_DIR/scripts/system.scpt
+while read -r setting; do
+  exec $setting
+done < $(find .macos -regex '.*\.defaults')
